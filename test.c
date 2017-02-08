1700fad5f96925e0a8c972f925e8c4f662d8abd3
Matthias <mail@matthiasbock.net>
2017-02-08 19:08:17 +0100

    more elegant struct initialization

--- test.c
+++ test.c
@@ -1,3 +1,13 @@
+/**
+ * Test program for various TUI elements
+ *
+ * Author: Matthias Bock <mail@matthiasbock.net>
+ * License: GNU GPLv3
+ *
+ * You are required to make all sourcecode public,
+ * which uses parts of this file. Otherwise you must
+ * contact the author and buy a license.
+ */
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -9,50 +19,76 @@
 #include "slider.h"
 #include "checkbox.h"
 
-box_t   main_window;
-label_t title;
-
-slider_t   slider;
-checkbox_t checkbox;
-
-void refresh()
+/**
+ * A simple test window with:
+ *  labels,
+ *  a slider and
+ *  a checkbox
+ */
+struct
 {
-    box_draw(&main_window);
-    label_draw(&title);
-    slider_draw(&slider);
-    checkbox_draw(&checkbox);
+
+    box_t      window;
+    label_t    title;
+    slider_t   slider;
+    checkbox_t checkbox;
+
+} test =
+{
+    .window =
+        {
+            .x1 = 1,
+            .y1 = 1,
+            .x2 = 80,
+            .y2 = 25,
+            .color = ANSI_FG_WHITE ANSI_BG_BLUE
+    	},
+    .title =
+        {
+            .x = 10,
+            .y = 3,
+            .text = "ucurses test program",
+            .color = ANSI_FG_CYAN ANSI_BG_BLUE
+        },
+    .slider =
+        {
+            .x1 = 10,
+            .x2 = 20,
+            .y  = 5,
+            .value_min = 1,
+            .value_max = 10,
+            .value = 5,
+            .color = ANSI_FG_BLUE ANSI_BG_WHITE
+        },
+    .checkbox =
+        {
+            .x = 10,
+            .y = 7,
+            .checked = true,
+            .color = ANSI_FG_WHITE ANSI_BG_BLUE
+        },
+};
+
+/**
+ * Draw all elements of this TUI
+ */
+void tui_draw()
+{
+    box_draw(&test.window);
+    label_draw(&test.title);
+    slider_draw(&test.slider);
+    checkbox_draw(&test.checkbox);
 }
 
+/**
+ * Main program
+ */
 int main()
 {
-    main_window.x1 = 1;
-    main_window.y1 = 1;
-    main_window.x2 = 80;
-    main_window.y2 = 25;
-    main_window.color = ANSI_FG_WHITE ANSI_BG_BLUE;
-
-    title.x = 10;
-    title.y = main_window.y1 + 2;
-    title.text = "ucurses test program";
-    title.color = ANSI_FG_CYAN ANSI_BG_BLUE;
-
-    slider.x1 = 10;
-    slider.x2 = 20;
-    slider.y  = title.y + 2;
-    slider.value_min = 1;
-    slider.value_max = 10;
-    slider.value = 5;
-    slider.color = ANSI_FG_BLUE ANSI_BG_WHITE;
-
-    checkbox.x = 10;
-    checkbox.y = slider.y + 2;
-    checkbox.checked = true;
-    checkbox.color = ANSI_FG_WHITE ANSI_BG_BLUE;
-
-    refresh();
+    tui_draw();
 
     // begin terminal below window
-    ansi_gotoxy(1, main_window.y2+1);
+    ansi_gotoxy(1, 26);
 
     // reset colors
     printf(ANSI_RESET);
