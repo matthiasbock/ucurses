8aa08017252515df02026106137713d86752d466
Matthias <mail@matthiasbock.net>
2017-03-08 16:25:43 +0100

    added ANSI codes for clear screen and move cursor

--- ansi.h
+++ ansi.h
@@ -4,33 +4,40 @@
  * https://en.wikipedia.org/wiki/ANSI_escape_code
  * http://bluesock.org/~willg/dev/ansi.html
  * http://ascii-table.com/ansi-escape-sequences-vt-100.php
+ *
+ * Author: Matthias Bock <mail@matthiasbock.net>
+ * This file is hereby released into the public domain.
  */
 
 #ifndef ANSI_H
 #define ANSI_H
 
-#define ESC             "\x1b"
+#define ESC                 "\x1b"
 
-#define ANSI_RESET      "\x1b[0m"
+#define ANSI_CLEAR_SCREEN   ESC "[2J"
 
-#define ANSI_FG_RED     "\x1b[31m"
-#define ANSI_FG_GREEN   "\x1b[32m"
-#define ANSI_FG_YELLOW  "\x1b[33m"
-#define ANSI_FG_BLUE    "\x1b[34m"
-#define ANSI_FG_MAGENTA "\x1b[35m"
-#define ANSI_FG_CYAN    "\x1b[36m"
-#define ANSI_FG_WHITE   "\x1b[37m"
+#define ANSI_RESET          ESC "[0m"
 
-#define ANSI_BG_BLACK   "\x1b[40m"
-#define ANSI_BG_RED     "\x1b[41m"
-#define ANSI_BG_GREEN   "\x1b[42m"
-#define ANSI_BG_YELLOW  "\x1b[43m"
-#define ANSI_BG_BLUE    "\x1b[44m"
-#define ANSI_BG_MAGENTA "\x1b[45m"
-#define ANSI_BG_CYAN    "\x1b[46m"
-#define ANSI_BG_WHITE   "\x1b[47m"
+#define ANSI_FG_RED         ESC "[31m"
+#define ANSI_FG_GREEN       ESC "[32m"
+#define ANSI_FG_YELLOW      ESC "[33m"
+#define ANSI_FG_BLUE        ESC "[34m"
+#define ANSI_FG_MAGENTA     ESC "[35m"
+#define ANSI_FG_CYAN        ESC "[36m"
+#define ANSI_FG_WHITE       ESC "[37m"
+
+#define ANSI_BG_BLACK       ESC "[40m"
+#define ANSI_BG_RED         ESC "[41m"
+#define ANSI_BG_GREEN       ESC "[42m"
+#define ANSI_BG_YELLOW      ESC "[43m"
+#define ANSI_BG_BLUE        ESC "[44m"
+#define ANSI_BG_MAGENTA     ESC "[45m"
+#define ANSI_BG_CYAN        ESC "[46m"
+#define ANSI_BG_WHITE       ESC "[47m"
 
 #define ANSI_BEGIN_GRAPHICAL "\e(0"
 #define ANSI_END_GRAPHICAL   "\e(B"
+
+#define ANSI_MOVE_CURSOR_TO(x, y)   ESC "[" y ";" x "H"
 
 #endif
